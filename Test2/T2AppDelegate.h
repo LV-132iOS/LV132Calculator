10bc2528e63822517f26da8d63456eae8b2ebe44
Andrii V <verbov1@gmail.com>
2014-11-28 14:18:37 +0200

    Вся прога що в мене є Логика + трошки аналізу вводу

---
+++ Test2/T2AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  T2AppDelegate.h
+//  Test2
+//
+//  Created by Andrii V. on 26.11.14.
+//  Copyright (c) 2014 Andrii V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface T2AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test2/T2ViewController.h
@@ -0,0 +1,23 @@
+//
+//  T2ViewController.h
+//  Test2
+//
+//  Created by Andrii V. on 26.11.14.
+//  Copyright (c) 2014 Andrii V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface T2ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *textf;
+@property (strong, nonatomic) IBOutlet UILabel *lab;
+@property (strong, nonatomic) IBOutlet UILabel *TestLabel;
+
+@property (nonatomic) BOOL DivadeZero;
+
+
+- (IBAction)calculate:(id)sender;
+
+@end
